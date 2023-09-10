f3cd94b699db397e9e3991f74d754ca6e1029703
JEFERSSON_QE <112734981+Manzanito20003@users.noreply.github.com>
2023-09-10 18:39:26 -0500

    Add files via upload
    docker falla

---
+++ DEMO_NEW/src/main/java/com/example/demo_new/playlist/domain/PlaylistRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo_new.playlist.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+}
